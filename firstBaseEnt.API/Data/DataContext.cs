ad0b30177170ff2befd1b90f080f4bc73cc9943f
nicholas chikore <nickchikore@gmail.com>
2020-06-22 22:32:01 +0100

    Commit changes

---
+++ firstBaseEnt.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using firstBaseEnt.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace firstBaseEnt.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
